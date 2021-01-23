75b5a56eafe607ba3b8534e061e84f2a2b41dff8
DESKTOP-D95G5CT\Shorobura <shorobyramax@gmail.com>
2021-01-23 21:55:10 +0200

    first commit

---
+++ ifstatement/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ifstatement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //int some = 7; 
+
+            //some = 7;
+
+            //Console.WriteLine(some);
+
+            //some = 5;
+
+            //string astr = Console.ReadLine();
+
+            //int a = int.Parse(astr);
+            //int b = int.Parse(Console.ReadLine());
+            //int result = a + b;
+
+            //Console.WriteLine("Result is: " + result + "!");
+
+            // + - * / %
+
+            //Console.WriteLine(11 % 3);
+
+            //bool flag = true;
+
+            int some = 5;
+
+            if (some > 3)
+            {
+                Console.WriteLine("Hello from If!");
+            }
+            else if (some > 4)
+            {
+                Console.WriteLine("Hello from else If!1");
+            }
+            else if (some > 5)
+            {
+                Console.WriteLine("Hello from else If!2");
+            }
+            else
+            {
+                Console.WriteLine("Hello else !");
+            }
+
+            /*> Hello! What is your name?
+             * > 
+             * 
+             * 
+             */
+        }
+    }
+}
